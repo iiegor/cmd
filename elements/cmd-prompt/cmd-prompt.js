e4b77eb940185a74b55fbe1133004a201e63fcdc
iiegor <iegormundo@hotmail.com>
2016-02-08 18:47:43 +0100

    Update source files

--- elements/cmd-prompt/cmd-prompt.js
+++ elements/cmd-prompt/cmd-prompt.js
@@ -1,33 +1,87 @@
 var path = require('path');
 var pty = null;
+var child_process = require('child_process');
 
 (() => {
   Polymer({
     is: 'cmd-prompt',
 
+    properties: {
+      status: {
+        type: String,
+        value: 'success',
+        observer: '_statusChanged'
+      }
+    },
+
     _onExecute(evt) {
-      if (evt.keyCode !== 13)
+      if (evt.keyCode !== 13) {
+        this._handleKey(evt.keyCode);
         return;
+      }
 
       if (!pty) {
         pty = require('ptyw.js');
       }
 
-      var term = pty.fork(this.cmdPath, ['/s', '/c', this.$.input.value], {
+      const args = ['/s', '/c', '"' + this.$.input.value + '"'];
+
+      /*const term = pty.spawn(this.cmdPath, args, {
         name: 'xterm-color',
         cols: 80,
         rows: 30,
         cwd: this.homePath,
         env: process.env
+      });*/
+
+      const term = child_process.spawn(this.cmdPath, args, {
+        cwd: process.cwd,
+        env: process.env,
+        windowsVerbatimArguments: true
       });
 
-      term.on('data', (data) => {
-        console.log('-> ' + data);
-        
-        document.querySelector('cmd-content').$.content.innerHTML += data;
-      });
-
+      // Empty prompt input
       this.$.input.value = '';
+
+      term.stdout.on('data', this._handleOutput.bind(this));
+      term.stderr.on('data', this._handleError.bind(this));
+
+      term.on('close', this._handleClose.bind(this));
+    },
+
+    _handleOutput(data) {
+      this.parentNode.appendOutput(window.escape(window.parse(data)));
+    },
+
+    _handleError(data) {
+      console.log('error', data.toString('utf8'));
+    },
+
+    _handleClose(code) {
+      console.debug('Command closed with', code);
+
+      this.status = code === 1 ? 'error' : 'success';
+    },
+
+    _statusChanged(newValue, oldValue) {
+      var el = this.$['status-icon'];
+
+      if (newValue === 'success') {
+        el.classList.remove('error');
+      } else {
+        el.classList.add('error');
+      }
+    },
+
+    _handleKey(code) {
+      switch (code) {
+        case 38:
+          console.log('History back');
+          break;
+        case 40:
+          console.log('History forward');
+          break;
+      }
     },
 
     get cmdPath() {
--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 (() => {
   const package = require('./package.json');
-  const ipc = require('electron').ipcRenderer;
+  const electron = require('electron');
+  const ipc = electron.ipcRenderer;
+  const shell = electron.shell;
+  const escapeHTML = require('escape-html');
 
   window.onload = function() {
     document.body.classList.add(process.platform);
   }
+
+  window.parse = function(buff) {
+    if (buff instanceof Buffer) {
+      for (var i = 0; i < buff.length; i++) {
+        var char = buff[i];
+        
+        switch (char) {
+          case 12:
+            buff = "";
+            break;
+        }
+      }
+    }
+
+    buff = escapeHTML(buff.toString()).split('\n');
+    return buff.join('<br>');
+  }
+
+  window.escape = function(str) {
+    var urlRegex = /(http|https):\/\/\w+(\.\w+)*(:[0-9]+)?\/?(\/[.\w]*)*/g;
+
+    str = str.replace(urlRegex, function (url) {
+      return '<a onclick="openExternal(\'' + url + '\'); return false;" href="">' + url + '</a>';
+    });
+
+    return str;
+  }
+
+  window.openExternal = shell.openExternal;
 
   function init() {
     process.versions.app = package.version;
